2a04523124638656748bd59f315e5957a6399d93
Somesh Thombare <someshthombre199@gmail.com>
2026-05-29 16:31:02 +0530

    classlist practice

---
+++ ClassList_propertyOn_DOM/ClassList.js
@@ -0,0 +1,15 @@
+// let heading = document.querySelector('h1');
+// heading.classList();// show class
+
+// // 1 ClassList.add(0)
+// heading.classList.add('text-color'); // add class on h1 Attributs
+
+// // 2 ClassList.remove()
+// heading.classList.remove('text-color'); //remove the class on the h1 attribut
+
+// ///3.ClassList.contain() 
+// heading.classList.contains('text-color'); // if is class is exit then return true eithr false
+
+// //4. ClassList.toggle()
+// heading.classList.toggle('text-color');// if the class is exist then return false adn remove either not exist return true and add the classo on heading
+
